1225e19ca266ab4abfa058815768d64c1b0fb205
Dima Vasilev <dima20067@yandex.ru>
2021-05-01 18:01:38 +0300

    Проверка почты и вопроса

--- myform.py
+++ myform.py
@@ -1,6 +1,16 @@
+import re
 from bottle import post, request
 
 @post('/home', method='post')
 def my_form():
     mail = request.forms.get('ADRESS')
-    return "Thanks! The answer will be sent to the mail %s" % mail
+    quest = request.forms.get('QUEST')
+    pattern=r"^[a-zA-Z0-9]{1,100}[@][a-z]{2,6}\.[a-z]{2,4}"
+    number_re=re.compile(pattern)
+
+    if len(quest) == 0:
+        return "Sorry, your quest incorrect!"
+    else if number_re.findall(mail):
+        return "Thanks! The answer will be sent to the mail %s" % mail
+    else:
+        return "Sorry, your mail incorrect!"
