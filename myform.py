4c48ab352bbe0f9ba07b81113ada861dc9d2d7ed
user1 <user1@A518-13>
2021-05-17 14:28:49 +0300

    Запись в файл (доделать дозапись)

--- myform.py
+++ myform.py
@@ -1,6 +1,9 @@
 import re
 import pdb
+import json
 from bottle import post, request
+
+questions = {}
 
 @post('/home', method='post')
 def my_form():
@@ -9,13 +12,18 @@ def my_form():
     pattern=r"^[a-zA-Z0-9]{1,100}[@][a-z]{2,6}\.[a-z]{2,4}"
     number_re=re.compile(pattern)
 
-    questions = {}
-
     if len(quest) == 0:
         return "Sorry, your quest incorrect!"
     elif number_re.findall(mail):
         questions[mail] = quest
-        pdb.set_trace()
+
+        if (mail in questions):
+            questions[mail].append = quest
+        else:
+            questions[mail] = quest
+        with open('data.txt', 'w') as outfile:
+            json.dump(questions, outfile)
+
         return "Thanks! The answer will be sent to the mail %s" % mail
     else:
         return "Sorry, your mail incorrect!"
