36c30d2608a0bb8e483dda0ec825f31a1aff4174
user1 <user1@A518-13>
2021-05-17 14:19:14 +0300

    Использование массива

--- myform.py
+++ myform.py
@@ -1,4 +1,5 @@
 import re
+import pdb
 from bottle import post, request
 
 @post('/home', method='post')
@@ -8,9 +9,14 @@ def my_form():
     pattern=r"^[a-zA-Z0-9]{1,100}[@][a-z]{2,6}\.[a-z]{2,4}"
     number_re=re.compile(pattern)
 
+    questions = {}
+
     if len(quest) == 0:
         return "Sorry, your quest incorrect!"
     elif number_re.findall(mail):
+        questions[mail] = quest
+        pdb.set_trace()
         return "Thanks! The answer will be sent to the mail %s" % mail
     else:
         return "Sorry, your mail incorrect!"
+    
